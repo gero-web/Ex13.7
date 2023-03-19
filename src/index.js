99dc77eb205a22ed39a73511dae7b2239def25dc
gero-web <sergeq198@yandex.ru>
2023-03-19 12:04:36 +0800

    emd

---
+++ src/index.js
@@ -0,0 +1,19 @@
+console.log("Hello World!");
+const table = document.getElementById('vivivod');
+fetch('http://localhost:3000/posts').then((res)=>{
+
+    if(res.ok){
+       res.json().then((json)=>{
+           for(let obj of json){
+                let row = document.createElement('tr') 
+                for(let prop in obj){
+                    let column = document.createElement('td') 
+                    column.innerText = obj[prop];
+                    row.appendChild(column);
+                }
+                table.appendChild(row);  
+            }   
+       });
+      
+    }
+})
